112b598f4fe37ef141c19cc0205560c5e20f0b0e
Chris3riel <chrisurpined@gmail.com>
2025-03-18 21:08:39 -0600

    test updated

--- src/prueba.py
+++ src/prueba.py
@@ -1,2 +1,5 @@
 def funcionPrueba(a, b):
     return a + b
+
+def funcionPrueba2(a, b):
+    return a - b
--- src/test/test_prueba.py
+++ src/test/test_prueba.py
@@ -3,3 +3,6 @@ from src.prueba import funcionPrueba
 
 def test_funcionPrueba():
     assert funcionPrueba(5, 5) == 10
+
+def test_funcionPrueba2():
+    assert funcionPrueba(10, 5) == 15
